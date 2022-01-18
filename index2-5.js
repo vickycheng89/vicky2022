710eee5f795b7029c8abc1b352424716025eac80
vickycheng89 <vickycheng89@gmail.com>
2022-01-18 12:00:08 +0800

    V1.1

---
+++ index2-5.js
@@ -0,0 +1,15 @@
+let n1 = 9;
+let n2 = 0.1;
+let n3 = 0.2;
+let n4 = n2 + n3;
+
+let n5 = '9';
+let n5 = 9;
+
+let s1 = '09215604'
+let s2='abc';
+
+let s3 ='\'國文\'';
+let s4 ='80';
+let s5 =`您的國文分數為'${S}
+console.log(s3);
