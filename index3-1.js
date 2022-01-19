2b0973573bda393e517f065dab0b3ea2c1964376
vickycheng89 <vickycheng89@gmail.com>
2022-01-19 11:06:27 +0800

    3-1

--- index3-1.js
+++ index3-1.js
@@ -5,6 +5,12 @@ if(scroe >=60){
     console.log("您的分數是不及格的!");
 }
 if(scroe >=90){
-    console.log("您的分數是A級!");}
-    else if (scroe>=80){
-        console.log("您的分數是B級!");}
+    console.log("您的分數是A級!");
+    }else if (scroe>=80){
+        console.log("您的分數是B級!");
+    }else if (scroe>=70){
+        console.log("您的分數是C級!");
+    }    else if (scroe>=60){
+        console.log("您的分數是D級!");
+    }    else {
+        console.log("您的分數是不及格!");}
---
+++ index3-4.js
@@ -0,0 +1,5 @@
+let x = 1000;
+for (let i = 0; i < x; i++) {
+    console.log(i);
+    // more statements
+ }
